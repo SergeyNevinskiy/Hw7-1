d630c5930a3ff05ce6297aa912a33cd970e6d4f3
SergeyNevinskiy <88575089+SergeyNevinskiy@users.noreply.github.com>
2021-10-23 22:26:20 +0300

    Hw7

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+
+// 1
+// const numOrStr = prompt('input number or string');
+// console.log(numOrStr)
+
+// if(numOrStr === null || numOrStr.trim() === '' || isNaN( +numOrStr ) ) {
+    // console.log("Что то пошло не так");
+// } else {
+    // console.log("Отлично")
+// }
+
+
+// 2
+
+// const numOrStr = prompt('input number or string');
+// console.log(numOrStr);
+
+// switch (true) {
+//     case numOrStr === null:
+//         alert("Данные срочно ввести");
+//         break;
+//     case numOrStr.trim() === "":
+//         alert("Данные, а не пустые строки");
+//         break;
+//     case isNaN(+numOrStr):
+//         alert("Обычное число введи")
+//         break;
+//     default:
+//         alert("ура, дошло");
+//         break;
+// }
+
+
